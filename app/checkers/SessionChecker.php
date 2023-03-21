4d6bc6396fdca6e4591cdcf62d3334945941a319
Roy Vermeulen <rvermeulen@eljakim.nl>
2023-03-21 17:04:09 +0100

    updated session checker using the new session utility's

--- app/checkers/SessionChecker.php
+++ app/checkers/SessionChecker.php
@@ -2,27 +2,33 @@
 
 namespace PHProx\Checkers;
 
+use PHProx\Utill\SessionUtil;
+
 class SessionChecker
 {
-  public function checkAndSendToLogin()
+  public function checkAndSendToLogin(): mixed
   {
     session_start();
-    if (!isset($_SESSION['userId'])) {
-      session_destroy();
-      header("Refresh: 0; url=/User/login");
-    } else {
-      return true;
-    }
+    // if (!isset($_SESSION['userId'])) {
+    //   session_destroy();
+    //   header("Refresh: 0; url=/User/loginSessionUtill
+    // } else {
+    //   return true;
+    // }
+
+    return !isset($_SESSION['userId']) ? SessionUtil::destroySessionAndSendToPage('/User/login') : true;
   }
 
-  public function checker()
+  public function checker(): mixed
   {
     session_start();
-    if (!isset($_SESSION['userId'])) {
-      session_destroy();
-      return false;
-    } else {
-      return true;
-    }
+    // if (!isset($_SESSION['userId'])) {
+    //   session_destroy();
+    //   return false;
+    // } else {
+    //   return true;
+    // }
+
+    return !isset($_SESSION['userId']) ? SessionUtil::destroySessionAndReturnFalse() : true;
   }
 }
